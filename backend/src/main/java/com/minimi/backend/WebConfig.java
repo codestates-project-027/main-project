714f437a3aaceed5d02e19446ecb8bcec9b5bc84
5witchkr <hob4419@gmail.com>
2022-09-13 16:50:39 +0900

    feat:cors

---
+++ backend/src/main/java/com/minimi/backend/WebConfig.java
@@ -0,0 +1,17 @@
+package com.minimi.backend;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry){
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedHeaders("*")
+                .allowedMethods("GET","POST");
+    }
+}
