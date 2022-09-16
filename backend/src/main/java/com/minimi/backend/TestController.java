b49e119263309b55c1eba507937dfffbeb0d21c5
5witchkr <hob4419@gmail.com>
2022-09-16 20:52:38 +0900

    feat: testcontroller

--- backend/src/main/java/com/minimi/backend/TestController.java
+++ backend/src/main/java/com/minimi/backend/TestController.java
@@ -3,14 +3,25 @@ package com.minimi.backend;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class TestController {
 
-    @GetMapping("home")
-    public String getHome() {
-        return "home";
+    @GetMapping("/get")
+    public ResponseEntity get() {
+        return new ResponseEntity("get 성공", HttpStatus.OK);
+    }
+    @PostMapping("/post")
+    public ResponseEntity post(@RequestBody String string) {
+        return new ResponseEntity("post 성공 data: "+string, HttpStatus.CREATED);
+    }
+    @PatchMapping("/patch")
+    public ResponseEntity patch(@RequestBody String string) {
+        return new ResponseEntity("patch 성공 data: "+string, HttpStatus.RESET_CONTENT);
+    }
+    @DeleteMapping("/delete")
+    public ResponseEntity delete() {
+        return new ResponseEntity("delete 성공", HttpStatus.NO_CONTENT);
     }
 }
