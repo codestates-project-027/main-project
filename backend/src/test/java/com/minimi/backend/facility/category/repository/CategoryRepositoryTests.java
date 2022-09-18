231b47ad83a3bd734263c00f41ac79422402e09a
5witchkr <hob4419@gmail.com>
2022-09-19 01:16:32 +0900

    feat: categoryRepository tests save

---
+++ backend/src/test/java/com/minimi/backend/facility/category/repository/CategoryRepositoryTests.java
@@ -0,0 +1,42 @@
+package com.minimi.backend.facility.category.repository;
+
+import com.minimi.backend.facility.category.domain.Category;
+import com.minimi.backend.facility.category.domain.CategoryRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+@DisplayName("CategoryRepository Tests")
+public class CategoryRepositoryTests {
+
+    @Autowired
+    private CategoryRepository categoryRepository;
+
+    @Nested
+    @DisplayName("CategoryRepositorySuccess Case")
+    public class successCategoryRepository {
+
+        @Test
+        @DisplayName("Success CategoryRepository Test1 -> save")
+        public void save() {
+            Category category = Category.builder()
+                    .categoryTitle("헬스")
+                    .categoryStatus("활성")
+                    .categoryCode("220901")
+                    .build();
+
+            Category result = categoryRepository.save(category);
+
+            assertThat(result, equalTo(category));
+        }
+    }
+}
