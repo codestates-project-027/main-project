8c723c579033e57462f1e0ba41329f71807317d9
5witchkr <hob4419@gmail.com>
2022-09-18 21:37:32 +0900

    feat: add patchTestCase * add patchSuccessCase * add patchFailCaseNull * add patchFailCaseNoContent

--- backend/src/main/java/com/minimi/backend/facility/category/domain/CategoryRepository.java
+++ backend/src/main/java/com/minimi/backend/facility/category/domain/CategoryRepository.java
@@ -2,5 +2,9 @@ package com.minimi.backend.facility.category.domain;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    Optional<Category> findByCategoryCode(String categoryCode);
 }
--- backend/src/test/java/com/minimi/backend/facility/category/service/CategoryServiceTests.java
+++ backend/src/test/java/com/minimi/backend/facility/category/service/CategoryServiceTests.java
@@ -5,24 +5,19 @@ package com.minimi.backend.facility.category.service;
 import com.minimi.backend.facility.category.domain.Category;
 import com.minimi.backend.facility.category.domain.CategoryDto;
 import com.minimi.backend.facility.category.domain.CategoryRepository;
-import com.minimi.backend.facility.category.service.CategoryService;
-import com.minimi.backend.facility.category.service.CategoryServiceImpl;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.*;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.jupiter.api.function.Executable;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.*;
 import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.BDDMockito.given;
-import static org.mockito.BDDMockito.then;
-import static org.mockito.Mockito.*;
+import static org.mockito.BDDMockito.*;
 
 @ExtendWith(MockitoExtension.class)
 public class CategoryServiceTests {
@@ -32,36 +27,99 @@ public class CategoryServiceTests {
 
     @InjectMocks
     private CategoryServiceImpl categoryService;
-    @Test
-    @DisplayName("postCategory Test")
-    public void postCategory() throws Exception {
-        Category category = Category.builder()
-                .categoryTitle("헬스")
-                .categoryStatus("활성")
-                .build();
-        given(categoryRepository.save(category)).willReturn(category);
 
-        CategoryDto.request categoryRequest = new CategoryDto.request("헬스","활성");
-        Category result = categoryService.postCategory(categoryRequest);
+    @Nested
+    @DisplayName("postCategory test")
+    class postCategory{
+        private Category category;
+        private CategoryDto.request CategoryDtoRequest;
+        @BeforeEach
+        public void setup(){
+            Category category = new Category(1L, "220901","헬스","활성");
+            CategoryDto.request CategoryDtoRequest = new CategoryDto.request("헬스","활성");
+        }
 
-        then(categoryRepository).should(times(1)).save(any());
-        assertThat(result, equalTo(category));
+        @Nested
+        @DisplayName("success postCategory case")
+        class successPostCategoryCase{
+            @Test
+            @DisplayName("success postCategory test 1 -> create")
+            public void successPostCategory() throws Exception {
+                given(categoryRepository.save(Mockito.any(Category.class))).willReturn(category);
+
+                Category result = categoryService.postCategory(CategoryDtoRequest);
+
+                then(categoryRepository).should(times(1)).save(any());
+                assertThat(result, equalTo(category));
+            }
+        }
+        @Nested
+        @DisplayName("fail postCategory case")
+        class failPostCategoryCase{
+            @Test
+            @DisplayName("fail postCategory test 1 -> null")
+            public void failPostCategoryNull() throws Exception {
+
+                Category result = categoryService.postCategory(CategoryDtoRequest);
+
+                assertThat(result, is(nullValue()));
+            }
+
+        }
 
     }
 
-    @Test
-    @DisplayName("patchCategory Test")
-    public void patchCategory() throws Exception{
-//        Category category = Category.builder()
-//                .categoryTitle("헬스")
-//                .categoryStatus("활성")
-//                .build();
-//        given(categoryRepository.save(category)).willReturn(category);
-//        String categoryCode = "001";
-//        CategoryDto.request categoryRequest = new CategoryDto.request("헬스","활성");
-//        Category result = categoryService.patchCategory(categoryRequest);
-//
-//        then(categoryRepository).should(times(1)).save(any());
-//        assertThat(result, equalTo(category));
+    @Nested
+    @DisplayName("patchCategory test")
+    class patchCategory {
+        private Category category;
+        private CategoryDto.request CategoryDtoRequest;
+        private String categoryCode;
+
+        @BeforeEach
+        public void setup(){
+            Category category = new Category(1L, "220901","헬스","활성");
+            CategoryDto.request CategoryDtoRequest = new CategoryDto.request("헬스","비활성");
+            String categoryCode = "220901";
+        }
+        @Nested
+        @DisplayName("success patchCategory case")
+        class successPatchCategoryCase{
+            @Test
+            @DisplayName("success patchCategory test 1 -> patch")
+            public void successPatchCategory() throws Exception{
+                given(categoryRepository.save(category)).willReturn(category);
+                given(categoryRepository.findByCategoryCode(Mockito.anyString())).willReturn(Optional.of(category));
+
+                Category result = categoryService.patchCategory(categoryCode,CategoryDtoRequest);
+
+                then(categoryRepository).should(times(1)).save(any());
+                then(categoryRepository).should(times(1)).findByCategoryCode(any());
+                assertThat(result, equalTo(category));
+            }
+        }
+        @Nested
+        @DisplayName("fail patchCategory case")
+        class failPatchCategoryCase{
+            @Test
+            @DisplayName("fail patchCategory test 1 -> null")
+            public void failPatchCategoryNull() throws Exception {
+                Category result = categoryService.patchCategory(categoryCode, CategoryDtoRequest);
+
+                assertThat(result, is(nullValue()));
+            }
+            @Test
+            @DisplayName("fail patchCategory test 2 -> noContent")
+            public void failPatchCategoryNoContent() throws Exception {
+                lenient().when(categoryRepository.findByCategoryCode("undefinedCode"))
+                        .thenThrow(new NullPointerException("noContent CategoryCode"));
+
+                NullPointerException exception = Assertions.assertThrows(NullPointerException.class, () -> {
+                    categoryService.patchCategory(categoryCode, CategoryDtoRequest);
+                });
+
+                assertThat(exception.getMessage(),equalTo("noContent CategoryCode"));
+            }
+        }
     }
 }
