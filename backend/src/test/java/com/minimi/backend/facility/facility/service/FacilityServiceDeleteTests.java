02c61b849cd3ca03c50171708626c6c0c68d8f1b
5witchkr <hob4419@gmail.com>
2022-09-20 17:18:55 +0900

    feat: add FacilityDelete test successCase

--- backend/src/test/java/com/minimi/backend/facility/facility/service/FacilityServiceDeleteTests.java
+++ backend/src/test/java/com/minimi/backend/facility/facility/service/FacilityServiceDeleteTests.java
@@ -1,7 +1,6 @@
 package com.minimi.backend.facility.facility.service;
 
 
-
 import com.minimi.backend.facility.facility.domain.facility.Facility;
 import com.minimi.backend.facility.facility.domain.facility.FacilityDto;
 import com.minimi.backend.facility.facility.domain.facility.FacilityRepository;
@@ -18,17 +17,21 @@ import org.springframework.context.ApplicationEventPublisher;
 
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Optional;
 
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
+import static org.hamcrest.Matchers.equalTo;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.BDDMockito.given;
 import static org.mockito.BDDMockito.then;
+import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.times;
 
 @ExtendWith(MockitoExtension.class)
 @DisplayName("postFacility test")
-public class FacilityServicePostTests {
+public class FacilityServiceDeleteTests {
 
     @Mock
     private FacilityRepository facilityRepository;
@@ -42,64 +45,61 @@ public class FacilityServicePostTests {
     @InjectMocks
     private FacilityServiceImpl facilityService;
 
-
     private Facility facility;
-    private FacilityDto.request facilityDtoReq;
+    private Long facilityId;
 
     @BeforeEach
     public void setup () {
+        facilityId = 1L;
         facility = new Facility(
                 1L,"미니미헬스장","대표이미지",
                 new ArrayList<>(Arrays.asList("이미지1", "이미지2")),
                 "시설정보","서울특별시 강남구","www.website.com",
                 "010-0000-0000","34.12345, 119.12345", 4,
                 new ArrayList<>(Arrays.asList("헬스", "PT")), FacilityStatus.PENDING
-                );
-        facilityDtoReq = new FacilityDto.request("미니미헬스장","대표이미지",
-                new ArrayList<>(Arrays.asList("이미지1", "이미지2")),
-                "시설정보","서울특별시 강남구", "www.website.com",
-                "0110-0000-0000","34.12345, 119.12345",
-                new ArrayList<>(Arrays.asList("헬스", "PT")));
+        );
     }
 
     @Nested
-    @DisplayName("success postFacility case")
-    class successPostFacilityCase {
+    @DisplayName("success deleteFacility case")
+    class successDeleteFacilityCase {
         @Test
-        @DisplayName("success postFacility test 1 -> create")
-        public void successPostFacility() throws Exception {
-            given(facilityCategoryCheckListener.checkExistsByCategoryCode(Mockito.anyString())).willReturn(true);
-            given(facilityRepository.save(Mockito.any(Facility.class))).willReturn(facility);
+        @DisplayName("success DeleteFacility test 1 -> delete")
+        public void successDeleteFacility() throws Exception {
+            given(facilityRepository.existsById(Mockito.anyLong())).willReturn(true);
+            given(facilityRepository.findById(Mockito.anyLong())).willReturn(Optional.ofNullable(facility));
+            doNothing().when(facilityRepository).delete(any(Facility.class));
 
-            Facility result = facilityService.postFacility(facilityDtoReq);
+            facilityService.deleteFacility(facilityId);
 
-            then(facilityRepository).should(times(1)).save(any());
-            assertThat(result, equalTo(facility));
+            then(facilityRepository).should(times(1)).delete(any());
+            then(facilityRepository).should(times(1)).delete(any());
+            then(facilityRepository).should(times(1)).delete(any());
         }
     }
     @Nested
-    @DisplayName("fail postFacility case")
-    class failPostFacilityCase {
+    @DisplayName("fail DeleteFacility case")
+    class failDeleteFacilityCase {
         @Test
-        @DisplayName("fail postFacility test 1 -> null")
+        @DisplayName("fail DeleteFacility test 1 -> ")
         public void failPostFacility() throws Exception {
-            given(facilityCategoryCheckListener.checkExistsByCategoryCode(Mockito.anyString())).willReturn(true);
-            Facility result = facilityService.postFacility(facilityDtoReq);
-
-            assertThat(result, is(nullValue()));
+//            given(facilityCategoryCheckListener.checkExistsByCategoryCode(Mockito.anyString())).willReturn(true);
+//            Facility result = facilityService.postFacility(facilityDtoReq);
+//
+//            assertThat(result, is(nullValue()));
         }
 
         @Test
-        @DisplayName("fail postFacility test 2 -> not found category")
-        public void failPostFacilityNoCategory() throws Exception {
-            given(facilityCategoryCheckListener.checkExistsByCategoryCode(Mockito.anyString()))
-                    .willReturn(false);
-
-            Exception exception = Assertions.assertThrows(Exception.class, () -> {
-                facilityService.postFacility(facilityDtoReq);
-            });
-
-            assertThat(exception.getMessage(), equalTo("Not Found Category"));
+        @DisplayName("fail DeleteFacility test 2 -> not found facility")
+        public void failDeleteFacilityNotFound() throws Exception {
+//            given(facilityCategoryCheckListener.checkExistsByCategoryCode(Mockito.anyString()))
+//                    .willReturn(false);
+//
+//            Exception exception = Assertions.assertThrows(Exception.class, () -> {
+//                facilityService.postFacility(facilityDtoReq);
+//            });
+//
+//            assertThat(exception.getMessage(), equalTo("Not Found Category"));
         }
     }
 }
