73481e305f685e9d8f06d3026e25e543b45b163b
random-olive <104327907+random-olive@users.noreply.github.com>
2022-09-23 18:11:04 +0900

    fix:imports

--- frontend/src/components/Bar/Navbars.jsx
+++ frontend/src/components/Bar/Navbars.jsx
@@ -1,12 +1,17 @@
-import { BiBell } from 'react-icons/bi';
-import { IoIosArrowDown } from 'react-icons/io';
-import { TopNavbarGlobal } from '../../styles/globalStyle/BarGlobalStyle';
-import { IconWrapper } from '../../styles/components/IconStyles';
-
 import { Link } from 'react-router-dom';
-import { BottomNavbarGlobal } from '../../styles/globalStyle/BarGlobalStyle';
-import { BottomNavIconStyle } from '../../styles/components/IconStyles';
+import { IoIosArrowDown } from 'react-icons/io';
 
+import {
+  TopNavbarGlobal,
+  BottomNavbarGlobal,
+} from '../../styles/globalStyle/BarGlobalStyle';
+
+import {
+  BottomNavIconStyle,
+  IconWrapper,
+} from '../../styles/components/IconStyles';
+
+import { BiBell } from 'react-icons/bi';
 import { HiHome } from 'react-icons/hi'; //HiOutlineHome
 import { BsPeople, BsPerson } from 'react-icons/bs'; //BsFillPeopleFill, BsFillPersonFill
 import { AiOutlineHeart } from 'react-icons/ai'; //AiFillHeart
