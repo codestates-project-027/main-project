ecf67759517ae4065eba836e47258d6ba3595b43
random-olive <104327907+random-olive@users.noreply.github.com>
2022-09-27 18:45:23 +0900

    stop

--- frontend/src/components/Map/MapContainer.jsx
+++ frontend/src/components/Map/MapContainer.jsx
@@ -1,18 +1,31 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Map } from 'react-kakao-maps-sdk';
 import { MAPSIZE } from '../../constants/style';
 import { data } from '../../constants/test-markers';
 import MarkerContainer from './MarkerContainer';
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
+import { getLocation } from '../../redux/slices/locationSlice';
 
-const MapContainer = ({location}) => {
-  
+//test
+
+const MapContainer = ({ location }) => {
+  const dispatch = useDispatch();
   const locationState = useSelector((state) => state.location);
-  console.log(locationState)
 
-  const [myLoca, setMyLoca] = useState({ lat: 33.450701, lng: 126.570667 });
+  const [myLoca, setMyLoca] = useState({
+    lat: locationState.currentLocation.latitude,
+    lng: locationState.currentLocation.longitude,
+  });
+  //33.450701, 126.570667
+  const resetLoca = () => {
+    dispatch(getLocation({ currentLocation: '3' }));
+    console.log(locationState);
+  };
+
   return (
     <>
+      <button onClick={resetLoca}>RESET</button>
+
       <Map
         center={myLoca}
         style={{
