38f3b6daa8272dbcd47ac2d1f43e6e2c75aa0472
random-olive <104327907+random-olive@users.noreply.github.com>
2022-09-27 22:51:20 +0900

    fix:get location from marker

--- frontend/src/components/Map/MapContainer.jsx
+++ frontend/src/components/Map/MapContainer.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
-import { Map } from 'react-kakao-maps-sdk';
+import { Map, MapMarker } from 'react-kakao-maps-sdk';
 import { MAPSIZE } from '../../constants/style';
 import { data } from '../../constants/test-markers';
-import MarkerContainer from './MarkerContainer';
+import MarkerContainer from './MarkerContainers';
 import { useSelector, useDispatch } from 'react-redux';
 import { getLocation } from '../../redux/slices/locationSlice';
 
@@ -11,14 +11,21 @@ import { getLocation } from '../../redux/slices/locationSlice';
 const MapContainer = ({ location }) => {
   const dispatch = useDispatch();
   const locationState = useSelector((state) => state.location);
+  const [main, setMain] = useState(true);
+  const [custom, setCustom] = useState(true);
+  const [customMarker, setCustomMarker] = useState();
 
   const [myLoca, setMyLoca] = useState({
     lat: locationState.currentLocation.latitude,
     lng: locationState.currentLocation.longitude,
   });
-  //33.450701, 126.570667
+
   const resetLoca = () => {
-    dispatch(getLocation({ currentLocation: '3' }));
+    dispatch(
+      getLocation({
+        currentLocation: { latitude: 33.450701, longitude: 126.570667 },
+      })
+    );
     console.log(locationState);
   };
 
@@ -34,7 +41,28 @@ const MapContainer = ({ location }) => {
           marginBottom: '20px',
         }}
         level={3}
+        onClick={
+          (_t, mouseEvent) =>
+            setCustomMarker({
+              lat: mouseEvent.latLng.getLat(),
+              lng: mouseEvent.latLng.getLng(),
+            })
+          //dispatch :: custom location coords
+        }
       >
+        {customMarker && (
+          <MarkerContainer
+            key={`MarkerCont-custom`}
+            position={customMarker}
+            custom={custom}
+          />
+        )}
+        <MarkerContainer
+          key={`MarkerCont-main`}
+          position={myLoca}
+          main={main}
+          content={'현재 위치'}
+        />
         {data.map((el) => (
           <MarkerContainer
             key={`MarkerCont-${el.latlng.lat}-${el.latlng.lng}`}
--- frontend/src/components/Map/MarkerContainers.jsx
+++ frontend/src/components/Map/MarkerContainers.jsx
@@ -2,31 +2,35 @@ import { useState } from 'react';
 import { useMap, MapMarker } from 'react-kakao-maps-sdk';
 import { MarkerIconStyle } from '../../styles/components/IconStyles';
 import { useSelector } from 'react-redux';
+import Pin from '../../assets/icon/map-pin.png';
 
-const MarkerContainer = ({ position, content }) => {
+const MarkerContainer = ({ position, content, main, custom }) => {
   const map = useMap();
   const [isHover, setIsHover] = useState(false);
   const locationState = useSelector((state) => state.location);
 
   return (
     <>
-      {/* <MapMarker // 내 위치 마커
-        position={{
-          lat: locationState.currentLocation.latitude,
-          lng: locationState.currentLocation.longitude,
-        }}
-        draggable={true}
-        onClick={(marker) => map.panTo(marker.getPosition())}
-
-      /> */}
-
       <MapMarker
         position={position}
         onClick={(marker) => map.panTo(marker.getPosition())}
-        onMouseOver={() => setIsHover(true)}
+        onMouseOver={main||custom ? () => setIsHover(false) : () => setIsHover(true)}
         onMouseOut={() => setIsHover(false)}
+        content={'current'}
+        image={
+          main
+            ? {
+                src: 'https://images.emojiterra.com/google/noto-emoji/v2.034/128px/1f535.png', // 마커이미지의 주소입니다
+                size: {
+                  width: 25,
+                  height: 25,
+                },
+              }
+            : ''//custom일 때 이미지 다르게
+        }
       >
         {isHover && <MarkerIconStyle>{content}</MarkerIconStyle>}
+        
       </MapMarker>
     </>
   );
